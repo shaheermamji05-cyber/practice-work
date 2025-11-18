a3baa4f1d3c16f461e55112d6d1be167fc50986c
Muhammad Shaheer <shaheermamji05@gmail.com>
2025-11-18 19:40:33 +0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,38 @@
+let firstname= prompt("Enter your Firstname")
+console.log(`Hello ${firstname}`)
+
+
+let lastname= prompt("Enter your Lastame")
+console.log(` ${lastname}`)
+
+
+let fullname= prompt("Enter your Fullname")
+console.log(` ${fullname}`)
+
+
+let age= prompt("Enter your age")
+console.log(`${age}`)
+
+
+let cityname= prompt("Enter your city name")
+console.log(`${cityname}`)
+
+
+let country= prompt("Enter your country name")
+console.log(` ${country}`)
+
+
+let collagename= prompt("Enter your collage name")
+console.log(`${collagename}`)
+
+
+let favoritesubject= prompt("Enter your favorite subject")
+console.log(` ${favoritesubject}`)
+
+
+let Dateofbirth= prompt("Enter your Date of birth")
+console.log(` ${Dateofbirth}`)
+
+
+let currentyear= prompt("Enter your current year")
+console.log(` ${currentyear}`)
